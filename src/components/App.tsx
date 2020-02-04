02790c8293734c9ec457450635cc30f077c866e0
Matt Black <mjblack23@gmail.com>
2020-02-04 11:41:12 -0500

    scaffolded functions in app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,31 @@ import * as React from 'react'
 import Todos from './Todos'
 
 export default class App extends React.Component{
+
+  appendTodo = () => {
+    // Todo
+  }
+
+  completeTodo = () => {
+    // Todo
+  }
+
+  deleteTodo = () => {
+    // Todo
+  }
+
+  updateSearch = () => {
+    // Todo
+  }
+
+  toggleView = () => {
+    
+  }
+
+  private static applySearch = () => {
+    // Todo
+  }
+
   render() {
     return (
       <section className="section">
